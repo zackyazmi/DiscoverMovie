660935939ea77d81f440e90c984450f877245ab1
Zacky Ilahi Azmi <zackyilahi.azmi@gmail.com>
2023-02-18 17:01:55 +0700

    Initial Commit

---
+++ DiscoverMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiscoverMovie
+//
+//  Created by Zacky Ilahi Azmi on 18/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
